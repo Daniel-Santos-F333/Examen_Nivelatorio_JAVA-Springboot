58418505e49d58df51997288c87c9ff606a49ceb
DanielSantos <cuentaanormal1208@gmail.com>
2026-04-05 22:28:01 -0500

    feat: :sparkles: adds the project's database

---
+++ BD/creates.sql
@@ -0,0 +1,28 @@
+create database examen_nivelatorio;
+
+use examen_nivelatorio;
+
+create table estudiantes(
+    id int not null auto_increment, 
+    nombre varchar(50) not null, 
+    email varchar(200) not null, 
+    edad int not null, 
+    primary key(id)
+);
+
+create table cursos(
+    id int not null auto_increment, 
+    nombre varchar(50) not null, 
+    descripcion varchar(200), 
+    fecha Date, 
+    activo boolean not null, 
+    primary key (id)
+);
+
+create table curso_estudiante(
+    id int not null auto_increment, 
+    curso_id int not null, 
+    estudiante_id int not null, 
+    foreign key (curso_id) references cursos(id), 
+    foreign key (estudiante_id) references estudiantes(id)
+);
